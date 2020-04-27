99b271f4354b2ae76aa65c4874558a87bc30afc4
alivanaki <ali.vanaki100@gmail.com>
2020-04-28 02:05:59 +0430

    Aamadan man

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Salam");
         System.out.println("Hello world");
+        System.out.println("man Oomadam");
     }
 }
